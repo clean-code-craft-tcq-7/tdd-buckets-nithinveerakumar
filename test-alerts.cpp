ffb29adcc1c74b8ac97fc80d4b93e273c2ff7553
ndt1cob <dhanakumar.nithinveerakumar@in.bosch.com>
2023-02-01 00:46:36 +0530

    commit 2 week 2

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "currentRangeCounter.cpp"
+#include "currentRangeCounter.hpp"
 
 //DDD
 map<string,string> TestData = {
